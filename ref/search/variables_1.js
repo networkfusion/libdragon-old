724373a9b33d5041f59b3c7e68adcbadc70050b0
networkfusion <networkfusion@users.noreply.github.com>
2022-04-01 20:28:45 +0000

    deploy: b242d719ee1b75976c6a5be3f02db4d191ecbd77

--- ref/search/variables_1.js
+++ ref/search/variables_1.js
@@ -5,7 +5,9 @@ var searchData=
   ['address_2',['address',['../group__lowlevel.html#a3aea9c8992d775f599ea5520cdfbb460',1,'AI_regs_t']]],
   ['ai_5fcallback_3',['AI_callback',['../group__interrupt.html#gafeab012ae992c1b832d27fa54ed75b03',1,'interrupt.c']]],
   ['alternate_4',['alternate',['../structAYEnvelope.html#a83cc06194995ba0ccac61a5fefc85760',1,'AYEnvelope']]],
-  ['attack_5',['attack',['../structAYEnvelope.html#ad32733abe56129ddcf068f8ee5d1e1b1',1,'AYEnvelope']]],
-  ['author_6',['author',['../ym64_8h.html#a294e33c591e9385f2dd616b2de3412bc',1,'ym64player_songinfo_t']]],
-  ['ay_7',['ay',['../ym64_8h.html#ac2f49185869569f91066b6f53627a941',1,'ym64player_t']]]
+  ['assert_5fhandler_5',['assert_handler',['../structrsp__ucode__t.html#a4e6b8b919f54b9af109d67cd6f851ebe',1,'rsp_ucode_t']]],
+  ['attack_6',['attack',['../structAYEnvelope.html#ad32733abe56129ddcf068f8ee5d1e1b1',1,'AYEnvelope']]],
+  ['attrs_7',['attrs',['../ym64_8c.html#a0903c9d1a7c649656236eca442908c8f',1,'ym5header']]],
+  ['author_8',['author',['../ym64_8h.html#a294e33c591e9385f2dd616b2de3412bc',1,'ym64player_songinfo_t']]],
+  ['ay_9',['ay',['../ym64_8h.html#ac2f49185869569f91066b6f53627a941',1,'ym64player_t']]]
 ];
