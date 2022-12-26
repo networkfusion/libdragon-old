fd3dcfee631159d37c8fef06aee5bf8d36a3bac2
Robin Jones <networkfusion@users.noreply.github.com>
2022-12-26 19:14:09 +0000

    Remove rpmdevtools It is not in the repo. Try using more global build command again.

--- tools/build-toolchain.sh
+++ tools/build-toolchain.sh
@@ -189,11 +189,14 @@ pushd binutils_compile_target
     --with-cpu=mips64vr4300 \
     --disable-werror
 make -j "$JOBS"
+# make install-strip || sudo make install-strip || su -c "make install-strip"
+
 # https://manpages.debian.org/bullseye/checkinstall/checkinstall.8.en.html
-# $build_command='checkinstall --default -D --pkgversion "0.0.1" ' #$BINUTILS_V --pkgname "n64-brew-libdragon-binutils-$BINUTILS_V" --maintainer "n64-brew"'
-# $build_command || sudo $build_command || su -c "$build_command"
-sudo checkinstall --default -D --pkgversion "$BINUTILS_V" --pkgname "n64brew-libdragon-binutils" --maintainer "n64brew" --nodoc --strip
-sudo checkinstall --default -R --pkgversion "$BINUTILS_V" --pkgname "n64brew-libdragon-binutils" --maintainer "n64brew" --nodoc --strip --install=no --fstrans=yes
+$build_command=checkinstall --default -D --pkgversion "$BINUTILS_V" --pkgname "n64brew-libdragon-binutils" --maintainer "n64brew" --nodoc --strip
+$build_command || sudo $build_command || su -c "$build_command"
+# sudo checkinstall --default -D --pkgversion "$BINUTILS_V" --pkgname "n64brew-libdragon-binutils" --maintainer "n64brew" --nodoc --strip
+# sudo mkdir -p /root/rpmbuild/SOURCES
+# sudo checkinstall --default -R --pkgversion "$BINUTILS_V" --pkgname "n64brew-libdragon-binutils" --maintainer "n64brew" --nodoc --strip --install=no --fstrans=yes
 popd
 
 # # Compile GCC for MIPS N64.
